3713e8f541058835d76af2ae437b40c7ef3cacf6
Balakrishnan-1 <bk4922813@gmail.com>
2025-10-30 18:15:08 +0530

    Fix : Updated the Code

---
+++ todo-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Container from './components/Main';
+function App() {
+  return (
+    <>
+    <div className='heading'>
+      <h1>Write your Tasks Below </h1>
+    </div>
+    <Container/></>
+  );
+}
+
+export default App;
--- todo-app/src/components/Main.jsx
+++ todo-app/src/components/Main.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 function Container(){
 const [tasks, setTasks] = useState([]);
-  const [inputValue, setInputValue] = useState('');
-
+const [inputValue, setInputValue] = useState('');
+const li =document.getElementsByClassName("li");
   const Add = () => {
     if (inputValue.trim() !== '') {
       setTasks([...tasks, inputValue]);
       setInputValue('');
     }};
 return(
+  <>
 <div className="Container">
     <div className="content">
-    <h1>Todo-List</h1>
 <input
           type="text"
           value={inputValue}
@@ -20,14 +20,14 @@ return(
         />
 <button onClick={Add}>Add</button>
     </div>
-    <div className="Todo-list" id="todo">
+</div>
+ <div className="Todo-list" id="todo">
         <ul>
           {tasks.map((task, index) => (
-            <li key={index}>{task}</li>
+            <li className="li" key={index}>{task}</li>
           ))}
         </ul>
-    </div>
-</div>
+    </div></>
 );
 }
 export default Container;
